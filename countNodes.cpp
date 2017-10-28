cb6013f7fb823b9fb9373dc06dc9829f0deb7bfc
Quang Huy Tran <32348171+tr4n@users.noreply.github.com>
2017-10-28 20:00:12 +0700

    count Nodes which have both left and right child

---
+++ countNodes.cpp
@@ -0,0 +1,18 @@
+int countNodes(struct BNode *root)
+{
+	if(root == NULL)
+	{
+		return 0; 
+	}
+	
+	
+	if(root -> left != NULL && root -> right != NULL)
+	{
+		return 1+ countBNodes(root -> left) + countBNodes(root -> right);
+	}
+	else
+	{
+		return countBNodes(root -> left) + countBNodes(root -> right);
+	}
+	
+}
