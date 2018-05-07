4ed6776cc5bf12a6afb9021fbdb8025714e3e03b
Quang Huy Tran <32348171+tr4n@users.noreply.github.com>
2018-05-07 10:47:07 +0700

    Update findMid.cpp

--- findMid.cpp
+++ findMid.cpp
@@ -17,3 +17,23 @@ Node findMid(Node *head)
 	else 	
 		return *temp1->pNext ;
 }
+//------------------------------------------------
+Node findMid(Node *head)
+{
+	
+	if(NULL == head -> pNext)	return *head; 
+	
+	Node *temp1 = head; // the pointer jump one step 
+	Node *temp2 = head; // the pointer jump two steps  
+	
+	while( temp2 -> pNext != head && temp2 -> pNext -> pNext != head )
+	{
+		temp1 = temp1 -> pNext; 
+		temp2 = temp2 -> pNext -> pNext; 
+	}
+	
+	if(temp2 -> pNext == head)
+		return *temp1 ; 
+	else 	
+		return *temp1->pNext ;
+}
